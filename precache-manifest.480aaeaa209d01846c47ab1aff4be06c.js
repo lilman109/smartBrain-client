364df3678683308256a1f7c4a3fa8bd9651a37b2
lilman109 <akira.yoshidasjsu@gmail.com>
2020-05-08 19:38:58 +0900

    Updates

---
+++ precache-manifest.480aaeaa209d01846c47ab1aff4be06c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd39ce2083877dada4f324cf1ca235e",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "13c2634b3413bec59608",
+    "url": "/facerecognitionapp/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "933ca8400710e6c0a7b6",
+    "url": "/facerecognitionapp/static/css/main.bb090dd4.chunk.css"
+  },
+  {
+    "revision": "13c2634b3413bec59608",
+    "url": "/facerecognitionapp/static/js/2.1b18e6ae.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognitionapp/static/js/2.1b18e6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933ca8400710e6c0a7b6",
+    "url": "/facerecognitionapp/static/js/main.2c84a0b9.chunk.js"
+  },
+  {
+    "revision": "6be8fbba9890a9d11f9b",
+    "url": "/facerecognitionapp/static/js/runtime-main.0421eba0.js"
+  }
+]);
